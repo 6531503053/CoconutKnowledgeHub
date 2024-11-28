ee30a19910be747afe6fc3c871ec3ab6a4da9376
WasitpolKuekkong <122779441+WasitpolKuekkong@users.noreply.github.com>
2024-11-28 13:29:39 +0700

    Init
    Navbar & Footer

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
+  </template>
+  <style>
+ * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
+  }
+  body{
+    height: 100%;
+    background-color: gray;
+  }
+</style>
